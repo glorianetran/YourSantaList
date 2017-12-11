5652365b6a93c5f8cd1d1293509ffd9c9238b833
Gloriane Tran <gtran@mac-626.local>
2017-12-11 13:03:30 -0800

    Initial Commit

---
+++ YourSantaList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YourSantaList
+//
+//  Created by Gloriane Tran on 12/11/17.
+//  Copyright © 2017 Gloriane Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
